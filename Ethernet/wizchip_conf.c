c5bea5b9555dca1a49d481fd246eeb3daaa42c3a
ATmega8 <liuyang.2014@foxmail.com>
2017-06-21 06:45:03 +0000

    fix bug

--- Ethernet/wizchip_conf.c
+++ Ethernet/wizchip_conf.c
@@ -163,22 +163,21 @@ _WIZCHIP  WIZCHIP =
       };
 */      
 _WIZCHIP  WIZCHIP =
-      {
-      _WIZCHIP_IO_MODE_,
-      _WIZCHIP_ID_ ,
-      wizchip_cris_enter,
-      wizchip_cris_exit,
+{
+	  _WIZCHIP_IO_MODE_,
+	  _WIZCHIP_ID_,
+	  wizchip_cris_enter,
+	  wizchip_cris_exit,
       wizchip_cs_select,
       wizchip_cs_deselect,
       //M20150601 : Rename the function 
       //wizchip_bus_readbyte,
       //wizchip_bus_writebyte
       wizchip_bus_readdata,
-      wizchip_bus_writedata,
-//    wizchip_spi_readbyte,
+      wizchip_bus_writedata
+//    wizchip_spi_readbyte
 //    wizchip_spi_writebyte
-      };
-
+};
 
 static uint8_t    _DNS_[4];      // DNS server ip address
 static dhcp_mode  _DHCP_;        // DHCP mode
